4801b204823de5a0146c87005691bd16d1fa75ab
Sarah <sarah372@gmail.com>
2017-10-30 19:11:01 -0700

    NEW FILES!!

--- ChatClient.java
+++ ChatClient.java
@@ -1,30 +1,55 @@
-    import java.rmi.*;
-    import java.rmi.server.*;
-    import java.util.*;
-     
-    public class ChatClient {
-    	public static void main (String[] argv) {
-    	    try {
-    		    	System.setSecurityManager(new RMISecurityManager());
-    		    	Scanner s=new Scanner(System.in);
-    		    	System.out.println("Enter Your name and press Enter:");
-    		    	String name=s.nextLine().trim();		    		    	
-    		    	ChatInterface client = new Chat(name);
-     
-    		    	ChatInterface server = (ChatInterface)Naming.lookup("rmi://localhost/ABC");
-    		    	String msg="["+client.getName()+"] got connected";
-    		    	server.send(msg);
-    		    	System.out.println("[System] Chat Remote Object is ready:");
-    		    	server.setClient(client);
-     
-    		    	while(true){
-    		    		msg=s.nextLine().trim();
-    		    		msg="["+client.getName()+"] "+msg;		    		
-    	    			server.send(msg);
-    		    	}
-     
-    	    	}catch (Exception e) {
-    	    		System.out.println("[System] Server failed: " + e);
-    	    	}
-    		}
-    }
+import java.net.*;
+import java.io.*;
+
+public class ChatClient
+{  private Socket socket              = null;
+   private DataInputStream  console   = null;
+   private DataOutputStream streamOut = null;
+
+   public ChatClient(String serverName, int serverPort)
+   {  System.out.println("Establishing connection. Please wait ...");
+      try
+      {  socket = new Socket(serverName, serverPort);
+         System.out.println("Connected: " + socket);
+         start();
+      }
+      catch(UnknownHostException uhe)
+      {  System.out.println("Host unknown: " + uhe.getMessage());
+      }
+      catch(IOException ioe)
+      {  System.out.println("Unexpected exception: " + ioe.getMessage());
+      }
+      String line = "";
+      while (!line.equals(".bye"))
+      {  try
+         {  line = console.readLine();
+            streamOut.writeUTF(line);
+            streamOut.flush();
+         }
+         catch(IOException ioe)
+         {  System.out.println("Sending error: " + ioe.getMessage());
+         }
+      }
+   }
+   public void start() throws IOException
+   {  console   = new DataInputStream(System.in);
+      streamOut = new DataOutputStream(socket.getOutputStream());
+   }
+   public void stop()
+   {  try
+      {  if (console   != null)  console.close();
+         if (streamOut != null)  streamOut.close();
+         if (socket    != null)  socket.close();
+      }
+      catch(IOException ioe)
+      {  System.out.println("Error closing ...");
+      }
+   }
+   public static void main(String args[])
+   {  ChatClient client = null;
+      if (args.length != 2)
+         System.out.println("Usage: java ChatClient host port");
+      else
+         client = new ChatClient(args[0], Integer.parseInt(args[1]));
+   }
+}
