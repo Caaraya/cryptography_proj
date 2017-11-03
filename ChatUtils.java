31fcfdbbed55ef5e15cc7e5f17f50ddd9c4e81a0
Caaraya <christina-araya@hotmail.com>
2017-11-03 13:11:25 -0700

    added public/private encryption

--- ChatUtils.java
+++ ChatUtils.java
@@ -2,17 +2,79 @@ package assignment3;
 
 import java.lang.*;
 import java.io.*;
-import java.security.MessageDigest;
+import java.security.*;
 import java.math.BigInteger;
-import java.security.PrivateKey;
+import java.nio.*;
+import javax.crypto.*;
+
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class ChatUtils{
+    Cipher pkCipher;
+    Cipher aesCipher;
 
-    private PrivateKey getPrivateFromKeyStore(String path, String passwd, String alias)
+    public ChatUtils() throws GeneralSecurityException {
+        pkCipher = Cipher.getInstance("RSA");
+        aesCipher = Cipher.getInstance("AES");
+    }
+
+    private PrivateKey getPrivateKey(String loc)
     {
-        PrivateKey key = null;
-        
+        PrivateKey privateKey = null;
+        try{
+            File path = new File(loc);
+            FileInputStream inputStream = new FileInputStream(path);
+            int pathlen = (int)path.length();
+            byte[] data = new byte[pathlen];
+            inputStream.read(data, 0, pathlen);
+            KeyFactory kf = KeyFactory.getInstance("RSA"); // or "EC" or whatever
+            privateKey = kf.generatePrivate(new PKCS8EncodedKeySpec(data));
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+       
+        return privateKey;
+    }
+
+    private PublicKey getPublicKey(String loc)
+    {
+        PublicKey key = null;
+        try 
+        {
+            File path = new File(loc);
+            FileInputStream inputStream = new FileInputStream(path);
+            int pathlen = (int)path.length();
+            byte[] data = new byte[pathlen];
+            inputStream.read(data, 0, pathlen);
+            KeyFactory kf = KeyFactory.getInstance("RSA"); // or "EC" or whatever
+            key = kf.generatePublic(new X509EncodedKeySpec(data));
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
         return key;
+    }
+
+    public byte[] encryptPublicRSA(String path, String msg) throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException
+    {
+        PublicKey key = getPublicKey(path);
+        byte[] str = null;
+        if (key == null) return str;
+        pkCipher.init(Cipher.ENCRYPT_MODE, key);
+        str = pkCipher.doFinal(msg.getBytes());
+        return str;
+    }
+
+    public byte[] decryptPrivateRSA(String path, String msg) throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException
+    {
+        PublicKey key = getPublicKey(path);
+        byte[] str = null;
+        if (key == null) return str;
+        pkCipher.init(Cipher.DECRYPT_MODE, key);
+        str = pkCipher.doFinal(msg.getBytes());
+        return str;
     }
 
     public String hashpass(String plainpass){
