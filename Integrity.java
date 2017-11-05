c136594ed18b6802ed89db1669c1e6e39ad43d11
Sarah <sarah372@gmail.com>
2017-11-04 22:53:11 -0800

    Added comments

--- Integrity.java
+++ Integrity.java
@@ -4,12 +4,22 @@ import javax.crypto.*;
 import java.security.*;
 import java.util.Arrays;
 
-public class Integrity{
+/**
+The Integrity class uses Message Authentication Codes (MAC) to provide an integrity check when a message
+is received. In order to use this, the client and server must share a secret key that is exclusively used
+to initialize their MACs. When a sender sends a message, they will sign the message to get a MAC data tag.
+This data tag will then be included with the message and sent to the receiver. The receiver will check the
+integrity of the message to determine if the information is sound.
+*/
+
+public class Integrity {
 
     private Mac mac;
     private Key key;
 
-    // Initialize MAC with a new AES key
+    /**
+    Initializes MAC with an AES key
+     */
     public Integrity() {
         try {
             mac = Mac.getInstance("HmacSHA1");
@@ -22,8 +32,10 @@ public class Integrity{
         }
     }
 
-    // Initialize MAC with input key
-    // Input: Key to initialize MAC with
+    /**
+    Initializes MAC with given key
+    @param key Key to initialize MAC
+     */
     public Integrity(Key key) {
         try {
             mac = Mac.getInstance("HmacSHA1");
@@ -36,8 +48,12 @@ public class Integrity{
         }
     }
 
-    // Input: message to sign with MAC
-    // Output: MAC data tag
+    /**
+    This method will create a MAC data tag that can be sent along
+    with its corresponding message in order to provide an integrity check
+    @param message The message that will be signed with MAC
+    @return MAC data tag that corresponds to the message
+    */
     public byte[] signMessage(String message) {
         try {
             return mac.doFinal(message.getBytes());
@@ -46,10 +62,13 @@ public class Integrity{
         }
     }
 
-    // TODO: throw exception if it is false
-    // Input:
-    // Output: Returns true if MAC tags are identical and therefore integrity is valid.
-    //         Otherwise, returns false
+    /**
+    This method will check the integrity of a message
+    @param message The message
+    @param dataTag The dataTag that corresponds to the message
+    @return Will return true if integrity check is valid
+    @throws InvalidIntegrityException if the integrity of the message is invalid
+    */
     public boolean checkIntegrity(String message, byte[] dataTag) throws InvalidIntegrityException {
         byte[] test = mac.doFinal(message.getBytes());
         boolean integrity = Arrays.equals(dataTag, test);
@@ -74,7 +93,10 @@ public class Integrity{
         }
     }
 
-    //Output: Key used to initialize MAC
+    /**
+    getter method for the key
+    @return Gets key used for MAC algorithm
+    */
     public Key getKey() {
         return key;
     }
@@ -151,7 +173,7 @@ public class Integrity{
     }
 }
 
-// Will be thrown when integrity is invalid
+// This exception will be thrown when integrity is invalid
 class InvalidIntegrityException extends Exception {
     public InvalidIntegrityException(String message) {
         super(message);
