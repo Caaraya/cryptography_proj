ac2e2d9d41b7768715f5718152cd6a6ba4773469
Sarah <sarah372@gmail.com>
2017-11-04 21:01:04 -0800

    Simplified integriy checking, added testing, removed data tag field

--- Integrity.java
+++ Integrity.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 public class Integrity{
 
     private Mac mac;
-    private byte[] tag;
     private Key key;
 
     // Initialize MAC with a new AES key
@@ -27,7 +26,7 @@ public class Integrity{
     // Input: Key to initialize MAC with
     public Integrity(Key macKey) {
         try{
-            mac = Mac.getInstance("HmacSHA");
+            mac = Mac.getInstance("HmacSHA1");
             key = macKey;
             mac.init(key);
         } catch (InvalidKeyException e) {
@@ -36,46 +35,63 @@ public class Integrity{
             throw new RuntimeException("Exception while generating MAC with HmacSHA1", e);
         }
     }
-
+/*
     // Updates MAC to use a new key
     // Input: Key to initialize MAC with
-    public changeKey(Key macKey) {
+    public void changeKey(Key macKey) {
         mac.reset(macKey);
     }
-
+*/
     //Output: Key used to initialize MAC
-    public getKey() {
+    public Key getKey() {
         return key;
     }
 
     // Input: message to sign with MAC
     // Output: MAC data tag
-    public byte[] SignMessage(String message) {
+    public byte[] signMessage(String message) {
         try {
-            tag = mac.doFinal(message.getBytes());
-            return tag;
+            return mac.doFinal(message.getBytes());
         } catch (IllegalStateException e) {
             throw new RuntimeException("MAC was not initialized when signing message");
         }
     }
 
-    // Input: MAC data tag to compare with
+    // Input: Compar data Tag of message with inputted dataTag
     // Output: Returns true if MAC tags are identical and therefore integrity is valid.
     //         Otherwise, returns false
-    public boolean CheckIntegrity(byte[] tag) {
-        return Arrays.equals(this.tag, tag);
+    public boolean checkIntegrity(String message, byte[] dataTag) {
+        return Arrays.equals(dataTag, mac.doFinal(message.getBytes()));
     }
 
     // TODO: remove before merge
     // Testing
     public static void main(String[] args) {
 
+        // --------------------- Initial setup ---------------------
+
         // create new MAC on client side
-        Integrity key = new Integrity();
+        Integrity clientMAC = new Integrity();
+        
+        // Send this key to server (ENCRYPT IT)
+        Key clientKey = clientMAC.getKey();
 
+        // Server makes MAC object
+        Integrity serverMAC = new Integrity(clientKey);
 
-        byte[] test = key.SignMessage("hello");
-        System.out.println(key.CheckIntegrity(test));
-        System.out.println(test);
+        // --------------------- Message sharing ---------------------
+
+        // Client types message and signs it
+        String clientMessage = "Hello!!";
+        byte[] clientDataTag = clientMAC.signMessage(clientMessage);
+
+        // Client sends message and dataTag to Server
+        // (Allowed to be unencrypted if Confidentiality is unchecked)
+        String serverMessage = clientMessage;
+        byte[] messageDataTag = clientDataTag;
+
+        // Server will check integrity of message by generating MAC data tag of message
+        // and then comparing that to the data tag that it received
+        // boolean output = serverMac.checkIntegrity(clientMessage, messageDataTag);
     }
 }
